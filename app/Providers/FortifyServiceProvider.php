4a83bcee6ef500379264f9c38b12342d59ef4cc9
Daniel <danielzavaletamego@gmail.com>
2024-08-19 15:46:53 -0500

    Añadiendo Rubros a la DB

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -34,21 +34,7 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
-
-        Fortify::authenticateUsing(function (Request $request) {
-            $user = User::where('email', $request->email)->first();
-    
-            if ($user && Hash::check($request->password, $user->password)) {
-                if ($user->primer_login) {
-                    $user->primer_login = false;
-                    $user->save();
-                    return redirect()->route('configuración-inicial');
-                }
-                return $user;
-            }
-            return null;
-        });
-        
+                
         RateLimiter::for('login', function (Request $request) {
             $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
 
